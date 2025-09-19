534f3b7229fd6873614511199759f4184a83adce
nbr <nikolaus.brunner@protonmail.ch>
2025-09-19 09:05:27 +0200

    refactor(config): simplify global configuration handling

--- lua/radar/init.lua
+++ lua/radar/init.lua
@@ -1,17 +1,15 @@
 local M = {}
 
-M.config = require("radar.config").default
-
 ---Setup function called by lazy.nvim or manual setup
 ---@param opts? Radar.Config
 ---@return nil
 function M.setup(opts)
   opts = opts or {}
-  M.config = vim.tbl_deep_extend("force", M.config, opts)
+  local config = vim.tbl_deep_extend("force", require("radar.config").default, opts)
 
-  require("radar.autocmd").setup(M.config)
-  require("radar.keys").setup(M.config)
-  require("radar.persistence").populate(M.config, require("radar.ui.mini_radar"))
+  require("radar.autocmd").setup(config)
+  require("radar.keys").setup(config)
+  require("radar.persistence").populate(config, require("radar.ui.mini_radar"))
 end
 
 return M
