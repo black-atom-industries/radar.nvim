cefcfa710feee9a72cb971002bd25001146cfc17
nbr <nikolaus.brunner@protonmail.ch>
2025-09-19 09:05:46 +0200

    refactor(autocmd): optimize autocmd setup and local reference usage

--- lua/radar/autocmd.lua
+++ lua/radar/autocmd.lua
@@ -2,8 +2,11 @@ local M = {}
 
 ---@param config Radar.Config
 function M.setup(config)
-  vim.api.nvim_create_autocmd("VimEnter", {
-    group = vim.api.nvim_create_augroup("radar.VimEnter", { clear = true }),
+  local autocmd = vim.api.nvim_create_autocmd
+  local autogrp = vim.api.nvim_create_augroup
+
+  autocmd("VimEnter", {
+    group = autogrp("radar.VimEnter", { clear = true }),
     callback = function()
       -- Update recent files now that vim.v.oldfiles is loaded
       local recent = require("radar.recent")
@@ -17,17 +20,16 @@ function M.setup(config)
     end,
   })
 
-  vim.api.nvim_create_autocmd("VimResized", {
-    group = vim.api.nvim_create_augroup("radar.VimResized", { clear = true }),
+  autocmd("VimResized", {
+    group = autogrp("radar.VimResized", { clear = true }),
     callback = function()
       local mini_radar = require("radar.ui.mini_radar")
       mini_radar.update(config)
     end,
   })
 
-  -- Autocmds
-  vim.api.nvim_create_autocmd("BufEnter", {
-    group = vim.api.nvim_create_augroup("radar.BufEnter", { clear = true }),
+  autocmd("BufEnter", {
+    group = autogrp("radar.BufEnter", { clear = true }),
     callback = function()
       local recent = require("radar.recent")
       local mini_radar = require("radar.ui.mini_radar")
@@ -46,8 +48,8 @@ function M.setup(config)
   local last_collision_check = 0
   local COLLISION_THROTTLE_MS = 50 -- Only check collision every 50ms
 
-  vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-    group = vim.api.nvim_create_augroup("radar.CursorMoved", { clear = true }),
+  autocmd({ "CursorMoved", "CursorMovedI" }, {
+    group = autogrp("radar.CursorMoved", { clear = true }),
     callback = function()
       local now = vim.uv.hrtime() / 1000000 -- Convert nanoseconds to milliseconds
       if now - last_collision_check >= COLLISION_THROTTLE_MS then
